7fa17a69d256767a8b0d8040a73176163cbec468
Ihor Orlenko <eaglyig@gmail.com>
2023-10-07 08:27:41 +0300

    Added dark mode option to setings

--- MindCurve/Controllers/MemoGameViewController.swift
+++ MindCurve/Controllers/MemoGameViewController.swift
@@ -12,7 +12,6 @@ class MemoGameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
     }
     
     
--- MindCurve/Controllers/SettingsViewController.swift
+++ MindCurve/Controllers/SettingsViewController.swift
@@ -9,10 +9,59 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    let appearanceLabel = UILabel()
+    let appearanceSwitcher = UISwitch()
+    let appearanceStack = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
+        setUpAppearanceStack()
+    }
+    
+    func setUpView() {
         view.backgroundColor = .systemBackground
         self.navigationItem.title = "Settings"
+    }
+    
+    func setUpAppearanceStack() {
+        appearanceLabel.text = "Dark mode appearance"
+        
+        appearanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        appearanceStack.addArrangedSubview(appearanceLabel)
+        
+        appearanceSwitcher.translatesAutoresizingMaskIntoConstraints = false
+        appearanceSwitcher.addTarget(self, action: #selector(appearanceShouldBeSwitched), for: .valueChanged)
+        appearanceStack.addArrangedSubview(appearanceSwitcher)
+        
+        appearanceStack.axis = .horizontal
+        appearanceStack.distribution = .fill
+        appearanceStack.alignment = .center
+        appearanceStack.spacing = 16
+        appearanceStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(appearanceStack)
+        
+        NSLayoutConstraint.activate([
+            appearanceStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            appearanceStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            appearanceStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
+    }
+    
+    @objc func appearanceShouldBeSwitched() {
+        if appearanceSwitcher.isOn {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                windowScene.windows.forEach { window in
+                    window.overrideUserInterfaceStyle = .dark
+                }
+            }
+        } else {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                windowScene.windows.forEach { window in
+                    window.overrideUserInterfaceStyle = .light
+                }
+            }
+        }
     }
 
 }
